adbf3f5c5d2287481c68181017a4692f3e6db872
alexander-chemeza <alexander.chemeza@gmail.com>
2021-03-09 07:47:24 +0300

    feat: adoptive sliders

--- js/main.js
+++ js/main.js
@@ -45,3 +45,99 @@ const prjSwiper = new Swiper('#project-slider', {
         }
     }
 })
+
+const testimonialsSwiper = new Swiper('#testimonials-slider', {
+    slidesPerView: 4,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.testimonials-slider-next',
+        prevEl: '.testimonials-slider-prev',
+      },
+    breakpoints: {
+        360: {
+            slidesPerView: 1
+        }, 
+        480: {
+            slidesPerView: 1
+        },
+        560: {
+            slidesPerView: 1
+        },
+        640: {
+            slidesPerView: 1
+        },
+        767: {
+            slidesPerView: 1
+        },
+        960: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+        1280: {
+            slidesPerView: 3
+        },
+        1366: {
+            slidesPerView: 3
+        },
+        1440: {
+            slidesPerView: 4
+        },
+        1600: {
+            slidesPerView: 4
+        },
+        1920: {
+            slidesPerView: 4
+        }
+    }
+})
+
+const testimonialsVideos = new Swiper('#testimonials-videos', {
+    slidesPerView: 2,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.video-prev',
+        prevEl: '.video-next',
+      },
+    breakpoints: {
+        360: {
+            slidesPerView: 1
+        }, 
+        480: {
+            slidesPerView: 1
+        },
+        560: {
+            slidesPerView: 1
+        },
+        640: {
+            slidesPerView: 1
+        },
+        767: {
+            slidesPerView: 1
+        },
+        960: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 2
+        },
+        1280: {
+            slidesPerView: 2
+        },
+        1366: {
+            slidesPerView: 2
+        },
+        1440: {
+            slidesPerView: 2
+        },
+        1600: {
+            slidesPerView: 2
+        },
+        1920: {
+            slidesPerView: 2
+        }
+    }
+})
